c325cffc8f330058071867585346ea0c5999e522
nowayrm <your.email@example.com>
2026-03-05 16:57:42 +0200

    some updates

--- crm_imtegration/start_bot.sh
+++ crm_imtegration/start_bot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /root/MovnaBot/crm_imtegration/myenv/bin/activate
-nohup python3 /root/MovnaBot/crm_imtegration/main.py > /dev/null 2>&1 &
+source /root/movna-test/crm_imtegration/myenv/bin/activate
+nohup python3 /root/movna-test/crm_imtegration/main.py > /dev/null 2>&1 &
 echo "Bot started"
